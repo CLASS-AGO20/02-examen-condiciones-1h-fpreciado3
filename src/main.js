643ab6a1f1458a5f440ca17a681cd0367911670d
fpreciado3 <fpreciado3@ucol.mx>
2020-11-26 15:17:32 -0600

    added the if sentences

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 export default class App {
     convertir(pesos) {
         let conversion;
+
+        if(pesos < 2000) {
+            conversion = pesos * 20.5;
+        } else {
+            conversion = pesos * 22.7;
+        }
     }
 
 }
